2c51bd9502e6cc529ad0f24b31b096c01f310ee0
MaddyBotha <35299442+MaddyBotha@users.noreply.github.com>
2018-08-08 22:33:01 +0200

    Rocket Added

--- js/rocket.js
+++ js/rocket.js
@@ -4,18 +4,35 @@ var countdownNumber = 10;
 
 var changeState = function (state) {
     document.body.className = 'body-state' + state;
-      clearInterval(timer);
-    
+    clearInterval(timer);
+    countdownNumber = 10;
+    document.getElementById('countdown').innerHTML = countdownNumber;
+
+    //countdown
     if (state == 2) {
-       timer = setInterval(function () {
-           document.getElementById('countdown').innerHTML = countdownNumber;
-           countdownNumber = countdownNumber - 1;
-           
-           if (countdownNumber <= 0) {
-             changeState(3);
-           }
-       },500); 
-    }else if (state == 3) {
-        
+        timer = setInterval(function () {
+                       countdownNumber = countdownNumber - 1;
+            document.getElementById('countdown').innerHTML = countdownNumber;
+
+
+            if (countdownNumber <= 0) {
+                changeState(3);
+            }
+        }, 500);
+    } else if (state == 3) {
+        var success = setTimeout(function () {
+            var randomNumber = Math.round(Math.random() * 10);
+
+            console.log('randomNumber:', randomNumber)
+
+            //SUCcESS
+            if (randomNumber > 5) {
+                // do something
+                changeState(4);
+            } else {
+                //do something else
+                changeState(5); // Oh no!!
+            }
+        }, 2000)
     }
 }
